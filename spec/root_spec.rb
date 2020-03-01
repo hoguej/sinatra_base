f4f76f353bb96f502e29dfc08147dfb9bf2cb937
Jon Hogue <jon.hogue@gmail.com>
2020-02-29 21:20:52 -0500

    A few more rspec cleanups

--- spec/root_spec.rb
+++ spec/root_spec.rb
@@ -1,5 +1,5 @@
-RSpec.describe "My Sinatra Application" do
-  it "should allow accessing the home page" do
+RSpec.describe 'My Sinatra Application' do
+  it 'should allow accessing the home page' do
     get '/'
 
     expect(last_response).to be_ok

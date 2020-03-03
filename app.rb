06b92a11c54a7ae2db4cc96c4141ab6bfdceaa75
Jon Hogue <jon.hogue@gmail.com>
2020-03-02 21:42:45 -0500

    Add some sample json responders

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require "json"
 require "sinatra"
 require "sinatra/activerecord"
 require "./models/content"
@@ -7,3 +8,41 @@ get "/" do
   @front_page = content ? content.body : "Hello Ohio"
   erb :index
 end
+
+# create content
+post "/content" do
+  content_type :json
+
+  data = JSON.parse(request.body.read)
+  
+  content = Content.new(name: data["name"], body: data["body"])
+
+  if !content.save
+    status 500
+    return {
+      errors: content.errors.full_messages
+    }.to_json
+  end
+
+  content.to_json
+end
+
+# view content
+get "/content/:id" do
+  content_type :json
+
+  content = Content.find(params["id"])
+
+  if !content
+    status 404
+    {
+      errors: ["Not Found"]
+    }.to_json
+  end
+
+  content.to_json
+end
+
+# list content
+# update content
+# delete content
